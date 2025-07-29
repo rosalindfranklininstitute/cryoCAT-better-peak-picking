0490c21b06ff98217457c8c5d20711676fa23789
DimitriosBellos <dimitrios.bellos@rfi.ac.uk>
2025-07-29 15:49:42 +0100

    debugging

--- cryocat/tmana.py
+++ cryocat/tmana.py
@@ -196,9 +196,11 @@ def scores_extract_particles(
         array_of_scores = np.array(list_of_scores)
         if local_threshold_diameter is not None:
             array_of_thresholds = np.array(list_of_thresholds)
+            print("list_of_thresholds shape:", list_of_thresholds.shape)
             filter = array_of_scores > array_of_thresholds
         else:
             filter = array_of_scores > threshold
+        print("filter shape:", filter.shape)
         filter = filter.tolist()
         list_of_scores = list(compress(list_of_scores, filter))
         list_of_coords = list(compress(list_of_coords, filter))
